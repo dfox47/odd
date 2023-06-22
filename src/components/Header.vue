fe34f8a746717fbabb852f2f1451a9afddca4041
dfox <dfox@foxartbox.com>
2023-06-22 18:16:13 +0300

    Header CSS

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,15 @@
 <template>
-	<div class="header">
-		Header
-	</div>
+	<header class="header">
+		<button class="btn btn--back"></button>
+
+		<a class="logo" href="/"><img class="logo__img" src="@/assets/i/logo.svg" alt="logo"></a>
+
+		<div class="header-title">RichyRich</div>
+
+		<div class="info-bell">
+			<span class="info-bell__number">2</span>
+		</div>
+	</header>
 </template>
 
 <script>
