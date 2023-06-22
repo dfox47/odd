20a5da22b3df461de935ef4c1097dc824c085cdc
dfox <dfox@foxartbox.com>
2023-06-22 20:32:57 +0300

    My team

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
-	<div class="search_wrapper">
-		<div class="wrapper">
-			<h2>Search teams</h2>
+	<div class="home">
+		<div class="search_wrapper">
+			<div class="wrapper">
+				<h2>Search teams</h2>
 
-			<div class="search">
-				<input class="search__input" type="text" placeholder="Search for a team">
+				<div class="search">
+					<input class="search__input" type="text" placeholder="Search for a team">
 
-				<button class="btn btn--close search__close"></button>
+					<button class="btn btn--close search__close"></button>
+				</div>
+			</div>
+		</div>
+
+		<div class="my_teams">
+			<div class="wrapper">
+				<h2>My teams</h2>
+
+				<div class="my_teams__empty">You aren't following any teams yet</div>
 			</div>
 		</div>
 	</div>
