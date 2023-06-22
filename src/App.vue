5e71e9eb848dac15254488450e7723ae8121f424
dfox <dfox@foxartbox.com>
2023-06-22 17:51:47 +0300

    Base vue 2

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="app">
+		<Header />
+		<router-view />
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
+
+export default {
+	name: 'App',
+	components: {
+		Footer,
+		Header
+	}
+}
+</script>
+
+<style lang="scss">@import '@/assets/styles/_styles.scss';</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="header">
+		Header
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Header',
+	data() {
+		return {}
+	},
+	mounted() {},
+	unmounted() {}
+}
+</script>
