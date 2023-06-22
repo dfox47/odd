5288e4f77a36c03169d146a348c7a89f3d42e1e8
dfox <dfox@foxartbox.com>
2023-06-22 20:18:34 +0300

    Search. Part 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
-	<div class="content">
-		content
+	<div class="search_wrapper">
+		<div class="wrapper">
+			<h2>Search teams</h2>
+
+			<div class="search">
+				<input class="search__input" type="text">
+
+				<button class="btn btn--close search__close"></button>
+			</div>
+		</div>
 	</div>
 </template>
