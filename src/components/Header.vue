eb34795df8084e3f5545d2fd779a7c886b6f2084
dfox <dfox@foxartbox.com>
2023-06-22 19:55:34 +0300

    Header CSS. Part 3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,32 @@
 <template>
-	<header class="header">
-		<div class="header-left">
-			<button class="btn btn--back"></button>
+	<div class="header_wrapper">
+		<header class="header">
+			<div class="header-left">
+				<button class="btn btn--back"></button>
 
-			<a class="logo" href="/"><img class="logo__img" src="@/assets/i/logo.svg" alt="logo"></a>
+				<a class="logo" href="/"><img class="logo__img" src="@/assets/i/logo.svg" alt="logo"></a>
+			</div>
+
+			<div class="header-title">RichyRich</div>
+
+			<div class="notification">
+				<span class="notification__number">2</span>
+			</div>
+		</header>
+
+		<div class="user-info">
+			<img class="user-info__img" src="@/assets/i/avatar.png" alt="">
+
+			<div class="user-info__level">Level 5</div>
 		</div>
 
-		<div class="header-title">RichyRich</div>
-
-		<div class="notification">
-			<span class="notification__number">2</span>
-		</div>
-	</header>
+		<nav class="nav">
+			<ul class="nav-list">
+				<li class="nav-list__item"><a class="nav-list__link active" href="/">My teams</a></li>
+				<li class="nav-list__item"><a class="nav-list__link" href="/">About</a></li>
+			</ul>
+		</nav>
+	</div>
 </template>
 
 <script>
